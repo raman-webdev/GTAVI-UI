7ead61e0e5b27e77f7911117223d89f632f09ab8
raman-webdev <chaudharyraman562@gmail.com>
2025-08-24 11:25:46 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='svg fixed top-0 left-0 -z-50 w-full h-screen overflow-hidden bg-black'>
+ 
+      </div>
+    </>
+  )
+}
+
+export default App
